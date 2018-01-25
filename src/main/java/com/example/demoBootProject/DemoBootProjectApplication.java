fd290fe41d6c527f3748e1e6454945e88bab4b32
rgbla <rgbla@DESKTOP-43I1PS3.home>
2018-01-26 00:01:24 +0100

    Add test controller

--- src/main/java/com/example/demoBootProject/DemoBootProjectApplication.java
+++ src/main/java/com/example/demoBootProject/DemoBootProjectApplication.java
@@ -2,11 +2,22 @@ package com.example.demoBootProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoBootProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoBootProjectApplication.class, args);
+	}
+
+	@RestController
+	class HomeController {
+
+		@GetMapping("/")
+		public String test() {
+			return "Hello FantasyManager!";
+		}
 	}
 }
