39cdd10f3e84ef8f288669a77e29cb76b791fdda
rgbla <rgbla@DESKTOP-43I1PS3.home>
2018-01-25 23:39:43 +0100

    Firs commit

---
+++ src/main/java/com/example/demoBootProject/DemoBootProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBootProjectApplication.class, args);
+	}
+}
